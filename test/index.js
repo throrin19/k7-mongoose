cda9e5d1e4e8522fcff291f311c4c02666f06e8e
Marcos Bérgamo <markusdarkness@gmail.com>
2016-03-07 22:26:07 -0300

    Apply semistandard

--- test/index.js
+++ test/index.js
@@ -6,11 +6,9 @@ const Lab = require('lab');
 const Code = require('code');
 const Hapi = require('hapi');
 const Path = require('path');
-const Sinon = require('sinon');
 
 // Load example
 
-const K7 = require('k7');
 const K7Mongoose = require('../lib');
 
 // Test shortcuts
@@ -57,7 +55,7 @@ describe('K7Mongoose', () => {
       expect(server.database).to.deep.include('User');
       expect(server.database).to.deep.include('Todo');
       done();
-    }); 
+    });
 
     after((done) => {
       server.database.mongoose.close(() => {
@@ -95,7 +93,7 @@ describe('K7Mongoose', () => {
       expect(server.database).to.deep.include('User');
       expect(server.database).to.deep.include('Todo');
       done();
-    }); 
+    });
 
     after((done) => {
       server.database.mongoose.close(() => {
@@ -135,7 +133,7 @@ describe('K7Mongoose', () => {
       expect(server.database).to.deep.include('User');
       expect(server.database).to.deep.include('Todo');
       done();
-    }); 
+    });
 
     after((done) => {
       server.database.mongoose.close(() => {
@@ -147,7 +145,7 @@ describe('K7Mongoose', () => {
   describe('when a plugin is registrated and query the model', () => {
     let server = new Hapi.Server();
     let error;
-    
+
     before((done) => {
       const register = {
         register: require('k7'),
@@ -166,94 +164,16 @@ describe('K7Mongoose', () => {
           throw err;
         }
 
+        // No error
+
+        expect(error).to.not.exist;
+
         expect(rows).to.be.an.array();
         expect(rows).to.be.empty();
 
         done();
       });
     });
-    
   });
-
-  // describe('when a plugin is registrated with a event on connected', () => {
-  //   let server = new Hapi.Server();
-  //   let error;
-  //   let connected = Sinon.spy();
-  //
-  //   options.events = {
-  //     connected: connected 
-  //   };
-  //
-  //   before((done) => {
-  //     const register = {
-  //       register: require('k7'),
-  //       options: options
-  //     };
-  //
-  //     server.register([register], (err) => {
-  //       error = err;
-  //       done();
-  //     });
-  //   });
-  //
-  //   it('should server have database populated', function (done) {
-  //     // No error
-  //
-  //     expect(error).to.not.exist;
-  //
-  //     // Instance of K7 should be registered
-  //     setTimeout(function () {
-  //       expect(connected.called).to.be.equal(true);
-  //       done(); 
-  //     }, 1000);
-  //   }); 
-  //
-  //   after((done) => {
-  //     server.database.mongoose.close(() => {
-  //       done();
-  //     });
-  //   });
-  // });
-  //
-  // describe('when a plugin is registrated with a event on disconnected', () => {
-  //   let server = new Hapi.Server();
-  //   let error;
-  //   let disconnect;
-  //
-  //   options.events = {
-  //     disconnected: function () {
-  //       disconnect = 'closed!';         
-  //     }
-  //   };
-  //
-  //   before((done) => {
-  //     const register = {
-  //       register: require('k7'),
-  //       options: options
-  //     };
-  //
-  //     server.register([register], (err) => {
-  //       error = err;
-  //       done();
-  //     });
-  //   });
-  //
-  //   before((done) => {
-  //     server.database.mongoose.close(() => {
-  //       done();
-  //     });
-  //   });
-  //
-  //   it('should server have database populated', (done) => {
-  //     // No error
-  //
-  //     expect(error).to.not.exist;
-  //
-  //     // Instance of K7 should be registered
-  //
-  //     expect(disconnect).to.be.equal('closed!');
-  //     done();
-  //   }); 
-  // });
 });
 
--- test/models/todo.js
+++ test/models/todo.js
@@ -19,4 +19,3 @@ const TodoModel = mongoose.model('Todo', Schema);
 
 module.exports = TodoModel;
 
-
