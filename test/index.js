e9434def414abe42d03937255307d3a64e68a9bb
Marcos Bérgamo <markusdarkness@gmail.com>
2016-09-24 14:22:11 -0300

    WIP: Implement k7 new interface

--- test/index.js
+++ test/index.js
@@ -21,159 +21,31 @@ const after = lab.after;
 const before = lab.before;
 
 describe('K7Mongoose', () => {
-  let options = {
-    connectionString: 'mongodb://localhost:27017/K7Mongoose',
-    models: 'test/models/*.js',
-    adapter: K7Mongoose
-  };
 
-  describe('when a plugin is registrated', () => {
-    let server = new Hapi.Server();
-    let error;
-    before((done) => {
-      const register = {
-        register: require('k7'),
-        options: options
-      };
+  describe('load', () => {
+    const options = {
+      connectionString: 'mongodb://localhost:27017/K7Mongoose',
+      models: 'test/models/*.js',
+    };
+    const k7Mongoose = new K7Mongoose(options);
 
-      server.register([register], (err) => {
-        error = err;
-        done();
-      });
-    });
+    it('returns the loaded models', (done) => {
+      k7Mongoose.load((err, db) => {
+        expect(err).to.not.exist;
+        expect(db).to.include('mongoose');
+        expect(db).to.include('Todo');
+        expect(db).to.include('User');
 
-    it('should server have database populated', (done) => {
-      // No error
-
-      expect(error).to.not.exist;
-
-      // Instance of K7 should be registered
-
-      expect(server).to.deep.include('database');
-      expect(server.database).to.be.an.object();
-      expect(server.database).to.deep.include('mongoose');
-      expect(server.database).to.deep.include('User');
-      expect(server.database).to.deep.include('Todo');
-      done();
-    });
-
-    after((done) => {
-      server.database.mongoose.close(() => {
-        done();
+        db.mongoose.close(() => {
+        
+          done();
+        });
       });
     });
   });
 
-  describe('when a plugin is registrated with an array of models in options', () => {
-    let server = new Hapi.Server();
-    let error;
-    options.models = ['test/models/user.js', 'test/models/todo.js'];
-    before((done) => {
-      const register = {
-        register: require('k7'),
-        options: options
-      };
-
-      server.register([register], (err) => {
-        error = err;
-        done();
-      });
-    });
-
-    it('should server have database populated', (done) => {
-      // No error
-
-      expect(error).to.not.exist;
-
-      // Instance of K7 should be registered
-
-      expect(server).to.deep.include('database');
-      expect(server.database).to.be.an.object();
-      expect(server.database).to.deep.include('mongoose');
-      expect(server.database).to.deep.include('User');
-      expect(server.database).to.deep.include('Todo');
-      done();
-    });
-
-    after((done) => {
-      server.database.mongoose.close(() => {
-        done();
-      });
-    });
-  });
-
-  describe('when a plugin is registrated with adapter as string', () => {
-    let server = new Hapi.Server();
-    let error;
-
-    options.adapter = Path.join(process.cwd(), './lib');
-
-    before((done) => {
-      const register = {
-        register: require('k7'),
-        options: options
-      };
-
-      server.register([register], (err) => {
-        error = err;
-        done();
-      });
-    });
-
-    it('should server have database populated', (done) => {
-      // No error
-
-      expect(error).to.not.exist;
-
-      // Instance of K7 should be registered
-
-      expect(server).to.deep.include('database');
-      expect(server.database).to.be.an.object();
-      expect(server.database).to.deep.include('mongoose');
-      expect(server.database).to.deep.include('User');
-      expect(server.database).to.deep.include('Todo');
-      done();
-    });
-
-    after((done) => {
-      server.database.mongoose.close(() => {
-        done();
-      });
-    });
-  });
-
-  describe('when a plugin is registrated and query the model', () => {
-    let server = new Hapi.Server();
-    let error;
-
-    before((done) => {
-      const register = {
-        register: require('k7'),
-        options: options
-      };
-
-      server.register([register], (err) => {
-        error = err;
-        done();
-      });
-    });
-
-    it('should have an empty array quering user', (done) => {
-      server.database.User.find({}, function (err, rows) {
-        if (err) {
-          throw err;
-        }
-
-        // No error
-
-        expect(error).to.not.exist;
-
-        expect(rows).to.be.an.array();
-        expect(rows).to.be.empty();
-
-        done();
-      });
-    });
+  describe('mongoose', () => {
+  
   });
 });
 
