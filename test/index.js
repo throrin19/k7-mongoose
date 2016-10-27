97dd474b9350ce6c8d11a07e34ccfa7b1dc50313
Marcos Bérgamo <marcos@thedon.com.br>
2016-10-27 21:15:39 -0200

    Fix tests

--- test/index.js
+++ test/index.js
@@ -4,7 +4,6 @@
 
 const Lab = require('lab');
 const Code = require('code');
-const Hapi = require('hapi');
 const Path = require('path');
 
 // Load example
@@ -17,123 +16,150 @@ const lab = exports.lab = Lab.script();
 const it = lab.it;
 const expect = Code.expect;
 const describe = lab.describe;
-const after = lab.after;
-const before = lab.before;
 
 describe('K7Mongoose', () => {
 
-  describe('load', () => {
+    describe('load', () => {
 
-    it('returns the loaded models', (done) => {
+        it('returns the loaded models', (done) => {
 
-      const options = {
-        connectionString: 'mongodb://localhost:27017/K7Mongoose',
-        models: 'test/models/*.js',
-      };
-      const k7Mongoose = new K7Mongoose(options);
+            const options = {
+                connectionString: 'mongodb://localhost:27017/K7Mongoose',
+                models: 'test/models/*.js',
+                events: {
+                    connected: () => {},
+                    disconnected: () => {},
+                    error: () => {}
+                }
+            };
 
-      k7Mongoose.load((err, db) => {
-        expect(err).to.not.exist;
-        expect(db).to.include('mongoose');
-        expect(db).to.include('Todo');
-        expect(db).to.include('User');
+            const k7Mongoose = new K7Mongoose(options);
 
-        db.mongoose.close(() => {
-        
-          return done();
+            k7Mongoose.load((err, db) => {
+
+                expect(err).to.not.exist();
+                expect(db).to.include('mongoose');
+                expect(db).to.include('Todo');
+                expect(db).to.include('User');
+
+                db.mongoose.close(() => {
+
+                    return done();
+                });
+            });
         });
-      });
-    });
 
-    it('returns the loaded models when passed an array', (done) => {
+        it('returns the loaded models when passed an array', (done) => {
 
-      const options = {
-        connectionString: 'mongodb://localhost:27017/K7Mongoose',
-        models: ['test/models/user.js', 'test/models/todo.js'],
-      };
-      const k7Mongoose = new K7Mongoose(options);
+            const options = {
+                connectionString: 'mongodb://localhost:27017/K7Mongoose',
+                models: ['test/models/user.js', 'test/models/todo.js'],
+                events: {
+                    connected: () => {},
+                    disconnected: () => {},
+                    error: () => {}
+                }
+            };
 
-      k7Mongoose.load((err, db) => {
-        expect(err).to.not.exist;
-        expect(db).to.include('mongoose');
-        expect(db).to.include('Todo');
-        expect(db).to.include('User');
+            const k7Mongoose = new K7Mongoose(options);
 
-        db.mongoose.close(() => {
-        
-          return done();
+            k7Mongoose.load((err, db) => {
+
+                expect(err).to.not.exist();
+                expect(db).to.include('mongoose');
+                expect(db).to.include('Todo');
+                expect(db).to.include('User');
+
+                db.mongoose.close(() => {
+
+                    return done();
+                });
+            });
         });
-      });
-    });
 
-    it('returns just mongoose when invalid path to models', (done) => {
+        it('returns just mongoose when invalid path to models', (done) => {
 
-      const options = {
-        connectionString: 'mongodb://localhost:27017/K7Mongoose',
-        models: '',
-      };
-      const k7Mongoose = new K7Mongoose(options);
+            const options = {
+                connectionString: 'mongodb://localhost:27017/K7Mongoose',
+                models: '',
+                events: {
+                    connected: () => {},
+                    disconnected: () => {},
+                    error: () => {}
+                }
+            };
 
-      k7Mongoose.load((err, db) => {
+            const k7Mongoose = new K7Mongoose(options);
 
-        expect(err).to.not.exist;
-        expect(db).to.include('mongoose');
-        expect(db).to.not.include('Todo');
-        expect(db).to.not.include('User');
+            k7Mongoose.load((err, db) => {
 
-        db.mongoose.close(() => {
-        
-          return done();
+                expect(err).to.not.exist();
+                expect(db).to.include('mongoose');
+                expect(db).to.not.include('Todo');
+                expect(db).to.not.include('User');
+
+                db.mongoose.close(() => {
+
+                    return done();
+                });
+            });
         });
-      });
-    });
 
-    it('returns only User\'s models when an absolute path is passed', (done) => {
-    
-      const options = {
-        connectionString: 'mongodb://localhost:27017/K7Mongoose',
-        models: Path.join(process.cwd(), 'test/models/user.js'),
-      };
-      const k7Mongoose = new K7Mongoose(options);
+        it('returns only User\'s models when an absolute path is passed', (done) => {
 
-      k7Mongoose.load((err, db) => {
-        expect(err).to.not.exist;
-        expect(db).to.include('mongoose');
-        expect(db).to.include('User');
+            const options = {
+                connectionString: 'mongodb://localhost:27017/K7Mongoose',
+                models: Path.join(process.cwd(), 'test/models/user.js'),
+                events: {
+                    connected: () => {},
+                    disconnected: () => {},
+                    error: () => {}
+                }
+            };
 
-        db.mongoose.close(() => {
-        
-          return done();
+            const k7Mongoose = new K7Mongoose(options);
+
+            k7Mongoose.load((err, db) => {
+
+                expect(err).to.not.exist();
+                expect(db).to.include('mongoose');
+                expect(db).to.include('User');
+
+                db.mongoose.close(() => {
+
+                    return done();
+                });
+            });
         });
-      });
+
+        it('returns an error when an invalid file is passed', (done) => {
+
+            const options = {
+                connectionString: 'mongodb://localhost:27017/K7Mongoose',
+                models: Path.join(process.cwd(), 'README.md')
+            };
+
+            const k7Mongoose = new K7Mongoose(options);
+
+            k7Mongoose.load((err, db) => {
+
+                expect(err).to.an.error(/README.md is not a valid model/);
+
+                return done();
+            });
+        });
     });
 
-    it('returns an error when an invalid file is passed', (done) => {
-    
-      const options = {
-        connectionString: 'mongodb://localhost:27017/K7Mongoose',
-        models: Path.join(process.cwd(), 'README.md'),
-      };
+    describe('mongoose', () => {
 
-      const k7Mongoose = new K7Mongoose(options);
+        const Mongoose = K7Mongoose.mongoose();
 
-      k7Mongoose.load((err, db) => {
-        console.log(err, db);
-        expect(err).to.not.exist;
+        it('returns the Mongoose Singleton', (done) => {
 
-        return done();
-      });
+            expect(Mongoose).to.be.an.object();
+
+            return done();
+        });
     });
-  });
-
-  describe('mongoose', () => {
-    const Mongoose = K7Mongoose.mongoose();
-  
-    it('returns the Mongoose Singleton', (done) => {
-      expect(Mongoose).to.be.an.object;
-
-      return done();
-    });
-  });
 });
 
--- test/models/user.js
+++ test/models/user.js
@@ -1,28 +1,28 @@
 'use strict';
 
-const mongoose = require('mongoose');
+const Mongoose = require('mongoose');
 
-const Schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  }
+const Schema = new Mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    }
 });
 
-const UserModel = mongoose.model('User', Schema);
+const UserModel = Mongoose.model('User', Schema);
 
 module.exports = UserModel;
